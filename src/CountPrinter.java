21c1f4e8819bb18cf1554b98ed236755647ab087
Sarah Haboeck <sarah.haboeck@htl-wels.at>
2021-09-30 14:27:18 +0200

    edit Main.main

--- src/CountPrinter.java
+++ src/CountPrinter.java
@@ -2,7 +2,7 @@ public class CountPrinter implements Runnable {
     @Override
     public void run() {
         for (int i = 0; i < 20; i++) {
-            System.out.println(i+1);
+            System.out.println(i+1+": ");
         }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
-
     public static void main(String[] args) {
+        DatePrinter dateprin = new DatePrinter();
+        CountPrinter countprin = new CountPrinter();
+
+        Thread threaddate = new Thread(dateprin);
+        Thread threadcount = new Thread(countprin);
+
+        threadcount.start();
+        threaddate.start();
 
     }
 }
